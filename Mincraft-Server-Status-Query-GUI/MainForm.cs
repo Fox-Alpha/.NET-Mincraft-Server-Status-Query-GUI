08cad960d535b24abb88cfcdee8690873f0eb964
Dirk.Buck <dirk.buck@alpha-com.de>
2015-11-23 11:22:36 +0100

    Add: SharpDev Projekt erstellt

---
+++ Mincraft-Server-Status-Query-GUI/MainForm.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: buck
+ * Datum: 23.11.2015
+ * Zeit: 11:19
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Mincraft_Server_Status_Query_GUI
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ Mincraft-Server-Status-Query-GUI/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: buck
+ * Datum: 23.11.2015
+ * Zeit: 11:19
+ * 
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Mincraft_Server_Status_Query_GUI
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
