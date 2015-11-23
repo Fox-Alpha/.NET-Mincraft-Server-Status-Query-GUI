5e15ae3ec166f76b3df7ad1a0629f0777041a1fc
Dirk.Buck <dirk.buck@alpha-com.de>
2015-11-23 11:59:42 +0100

    Add: Einfaches GUI Layout zum Testen der Abfrage erstellt

--- Mincraft-Server-Status-Query-GUI/MainForm.Designer.cs
+++ Mincraft-Server-Status-Query-GUI/MainForm.Designer.cs
@@ -17,6 +17,14 @@ namespace Mincraft_Server_Status_Query_GUI
 		private System.Windows.Forms.ToolStripMenuItem programmToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
 		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button2;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -43,6 +51,14 @@ namespace Mincraft_Server_Status_Query_GUI
 			this.programmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
 			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -78,11 +94,87 @@ namespace Mincraft_Server_Status_Query_GUI
 			this.statusStrip1.TabIndex = 1;
 			this.statusStrip1.Text = "statusStrip1";
 			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(496, 44);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(114, 37);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Status !";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(27, 64);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(153, 20);
+			this.textBox1.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(27, 48);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(153, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Serveradresse oder IP Adresse";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(212, 48);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(95, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Serverport (65565)";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(212, 64);
+			this.textBox2.MaxLength = 5;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 6;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(27, 141);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(582, 264);
+			this.richTextBox1.TabIndex = 7;
+			this.richTextBox1.Text = "";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(27, 115);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(73, 13);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "Server Output";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(534, 115);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 9;
+			this.button2.Text = "Clear";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(652, 438);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.statusStrip1);
 			this.Controls.Add(this.menuStrip1);
 			this.MainMenuStrip = this.menuStrip1;
