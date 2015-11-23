8ff61c2f95aa5ed738701170aced4142563ac826
Dirk.Buck <dirk.buck@alpha-com.de>
2015-11-23 11:25:40 +0100

    Add: Hauptmenü Add: Statuszeile Upd: Anwendungsinformationen

--- Mincraft-Server-Status-Query-GUI/MainForm.Designer.cs
+++ Mincraft-Server-Status-Query-GUI/MainForm.Designer.cs
@@ -13,6 +13,10 @@ namespace Mincraft_Server_Status_Query_GUI
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem programmToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
+		private System.Windows.Forms.StatusStrip statusStrip1;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -35,12 +39,60 @@ namespace Mincraft_Server_Status_Query_GUI
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.programmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.programmToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(652, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// programmToolStripMenuItem
+			// 
+			this.programmToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.beendenToolStripMenuItem});
+			this.programmToolStripMenuItem.Name = "programmToolStripMenuItem";
+			this.programmToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+			this.programmToolStripMenuItem.Text = "Programm";
+			// 
+			// beendenToolStripMenuItem
+			// 
+			this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
+			this.beendenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.beendenToolStripMenuItem.Text = "Beenden";
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Location = new System.Drawing.Point(0, 416);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(652, 22);
+			this.statusStrip1.TabIndex = 1;
+			this.statusStrip1.Text = "statusStrip1";
 			// 
 			// MainForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Mincraft-Server-Status-Query-GUI";
+			this.ClientSize = new System.Drawing.Size(652, 438);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "MainForm";
+			this.Text = "Mincraft-Server-Status-Query-GUI";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 	}
 }
--- Mincraft-Server-Status-Query-GUI/Properties/AssemblyInfo.cs
+++ Mincraft-Server-Status-Query-GUI/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Mincraft-Server-Status-Query-GUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tarnhorn Gaming Tools")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tarnhorn Software")]
 [assembly: AssemblyProduct("Mincraft-Server-Status-Query-GUI")]
 [assembly: AssemblyCopyright("Copyright 2015")]
 [assembly: AssemblyTrademark("")]
